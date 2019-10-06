daea83f8c3c332cbbfb93f02ae12241f9b8ebd98
RUARZ <44531288+RUARZ@users.noreply.github.com>
2019-10-06 18:55:16 +0200

    -) added Linker for ListView and GridViewColumn. NOTE: create binding of GridViewColumn is not working right now! Creating a ListView without ListView.View and GridView works fine.

---
+++ src/Yggdrasil.Wpf/Linker/GridViewColumnLinker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Yggdrasil.Wpf.Linker
+{
+    [RegisterLinker(typeof(GridViewColumn))]
+    public class GridViewColumnLinker : ILinker
+    {
+        #region Interface Implementation
+
+        public void Link(object control, object context, Dictionary<string, string> linkDefinitions, Dictionary<string, MemberInfo> foundLinks, Action<object, object, string> createLinkAction)
+        {
+            if (!(control is GridViewColumn gridViewColumn))
+                return;
+
+            foreach(KeyValuePair<string, string> definition in linkDefinitions)
+            {
+                switch(definition.Key)
+                {
+                    case nameof(GridViewColumn.DisplayMemberBinding):
+                        Binding binding = new Binding(definition.Value);
+                        binding.Source = context;
+                        gridViewColumn.DisplayMemberBinding = binding;
+                        break;
+                    default:
+                        throw new NotSupportedException($"Property '{definition.Key}' is not supported by {GetType().Name}!");
+                }
+            }
+        }
+
+        public void Unlink()
+        {
+            
+        }
+
+        #endregion
+    }
+}
---
+++ src/Yggdrasil.Wpf/Linker/ListViewLinker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Yggdrasil.Wpf.Linker
+{
+    [RegisterLinker(typeof(ListView))]
+    public class ListViewLinker : ILinker
+    {
+        #region Private Fields
+
+        private ItemsControlLinkHelper _linkHelper;
+
+        #endregion
+
+        #region Interface Implementations
+
+        public void Link(object control, object context, Dictionary<string, string> linkDefinitions, Dictionary<string, MemberInfo> foundLinks, Action<object, object, string> createLinkAction)
+        {
+            if (!(control is ListView listView))
+                return;
+
+            foreach (KeyValuePair<string, MemberInfo> link in foundLinks)
+            {
+                switch(link.Key)
+                {
+                    case nameof(ListView.ItemsSource):
+                        BindingOperations.SetBinding(listView, ItemsControl.ItemsSourceProperty, CreateBinding(link.Value.Name, context));
+
+                        if (listView.View is GridView view)
+                        {
+                            foreach (GridViewColumn col in view.Columns)
+                            {
+                                createLinkAction(col, context, col.GetValue(FrameworkElement.NameProperty).ToString());
+                            }
+                        }
+                        break;
+                    case nameof(ListView.SelectedItem):
+                        BindingOperations.SetBinding(listView, ListBox.SelectedItemProperty, CreateBinding(link.Value.Name, context));
+                        break;
+                    default:
+                        throw new NotSupportedException($"Property '{link.Key}' is not supported by {GetType().Name}!");
+                }
+            }
+        }
+
+        public void Unlink()
+        {
+            _linkHelper?.Dispose();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Binding CreateBinding(string propertyName, object context)
+        {
+            Binding binding = new Binding(propertyName);
+
+            if (context != null)
+                binding.Source = context;
+
+            return binding;
+        }
+
+        #endregion
+    }
+}
