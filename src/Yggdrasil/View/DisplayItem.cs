7e7ebf848d2410e727d16d154ccf9588aff50a82
RUARZ <44531288+RUARZ@users.noreply.github.com>
2019-10-23 22:40:48 +0200

    -) refactored DisplayItem and moved link creation and text resource setting to new class RuleExecutor

--- src/Yggdrasil/View/DisplayItem.cs
+++ src/Yggdrasil/View/DisplayItem.cs
@@ -15,10 +15,8 @@ namespace Yggdrasil
     {
         #region Private Methods
 
-        private readonly List<ILinker> _linkers = new List<ILinker>();
-        private readonly List<(FieldInfo FieldInfo, TextResourceKeyRule KeyRule, PropertyInfo PropertyInfo)> _resourceFields = new List<(FieldInfo FieldInfo, TextResourceKeyRule KeyRule, PropertyInfo PropertyInfo)>();
-        private (TextResourceKeyRule KeyRule, PropertyInfo PropertyInfo)? _viewResourceField;
-        private CultureChangedSubscription _cultureChangedSubscription;
+        private readonly RuleExecutor _ruleExecutor;
+        private readonly CultureChangedSubscription _cultureChangedSubscription;
 
         #endregion
 
@@ -33,7 +31,11 @@ namespace Yggdrasil
         {
             View = view;
             ViewModel = viewModel;
-            SetFieldActions();
+            _ruleExecutor = new RuleExecutor();
+            _ruleExecutor.CreateLinks(view, viewModel);
+            _ruleExecutor.SetResources(view);
+
+            _cultureChangedSubscription = CultureManager.Subscribe(culture => _ruleExecutor.SetResources(View));
         }
 
         #endregion
@@ -51,7 +53,7 @@ namespace Yggdrasil
 
         #endregion
 
-        #region Public Methods
+        #region Interface Implementation
 
         public void Dispose()
         {
@@ -62,151 +64,7 @@ namespace Yggdrasil
                 disposableViewModel.Dispose();
 
             _cultureChangedSubscription?.Dispose();
-
-            foreach (ILinker linker in _linkers)
-            {
-                linker.Unlink();
-            }
-        }
-
-        #endregion
-
-        #region Private Methods
-
-        private void SetFieldActions()
-        {
-            if (!RuleProvider.AreLinkRulesDefined && !RuleProvider.AreTextResourceKeyRulesDefined)
-                return;
-
-            foreach (FieldInfo viewFieldInfo in View.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
-            {
-                if (RuleProvider.AreLinkRulesDefined)
-                    AddLink(viewFieldInfo);
-
-                if (RuleProvider.AreTextResourceKeyRulesDefined)
-                    AddResourceFields(viewFieldInfo);
-            }
-
-            if (RuleProvider.AreTextResourceKeyRulesDefined)
-            {
-                TextResourceKeyRule rule = RuleProvider.GetTextResourceKeyRule(View.GetType());
-
-                if (rule != null)
-                {
-                    PropertyInfo info = View.GetType().GetProperty(rule.ResourcePropertyName);
-
-                    if (info != null)
-                        _viewResourceField = (rule, info);
-                }
-
-                _cultureChangedSubscription = CultureManager.Subscribe(info => SetResourceValues());
-                SetResourceValues();
-            }
-        }
-
-        private void AddLink(FieldInfo viewFieldInfo)
-        {
-            List<LinkRule> linkRules = RuleProvider.GetLinkRulesForType(viewFieldInfo.FieldType);
-
-            if (linkRules.Count <= 0)
-                return;
-
-            ILinker linker = LinkerManager.GetLinkerForType(viewFieldInfo.FieldType);
-
-            if (linker == null)
-                return;
-
-            Link(viewFieldInfo.GetValue(View), ViewModel, linkRules, linker, viewFieldInfo.Name);
-        }
-
-        private void CreateLink(object control, object context, string controlName)
-        {
-            List<LinkRule> linkRules = RuleProvider.GetLinkRulesForType(control.GetType());
-
-            if (linkRules.Count <= 0)
-                return;
-
-            ILinker linker = LinkerManager.GetLinkerForType(control.GetType());
-
-            if (linker == null)
-                return;
-
-            Link(control, context, linkRules, linker, controlName);
-        }
-
-        private void Link(object control, object context, IEnumerable<LinkRule> linkRules, ILinker linker, string controlName)
-        {
-            Type contextType = context?.GetType();
-
-            Dictionary<string, MemberInfo> foundLinks = new Dictionary<string, MemberInfo>();
-            Dictionary<string, string> linkDefinitions = new Dictionary<string, string>();
-
-            foreach (LinkRule rule in linkRules)
-            {
-                MemberInfo info = null;
-                string linkInfoName = rule.GetLinkInfoName(controlName);
-
-                switch (rule.RuleType)
-                {
-                    case LinkRuleType.Property:
-                        info = !string.IsNullOrEmpty(linkInfoName) ? contextType?.GetProperty(linkInfoName) : null;
-                        break;
-                    case LinkRuleType.Event:
-                        info = contextType?.GetMethod(linkInfoName);
-                        break;
-                }
-
-                linkDefinitions.Add(rule.InfoName, linkInfoName);
-                if (info != null)
-                    foundLinks.Add(rule.InfoName, info);
-            }
-
-            _linkers.Add(linker);
-            linker.Link(control, context, linkDefinitions, foundLinks, CreateLink);
-        }
-
-        private void AddResourceFields(FieldInfo viewFieldInfo)
-        {
-            TextResourceKeyRule rule = RuleProvider.GetTextResourceKeyRule(viewFieldInfo.FieldType);
-
-            if (rule == null)
-                return;
-
-            PropertyInfo propInfo = viewFieldInfo.FieldType.GetProperty(rule.ResourcePropertyName);
-
-            if (propInfo == null)
-                return;
-
-            _resourceFields.Add((viewFieldInfo, rule, propInfo));
-        }
-
-        private void SetResourceValues()
-        {
-            if (_viewResourceField != null)
-            {
-                _viewResourceField.Value.PropertyInfo
-                    .SetValue(View,
-                    ResourceHandler.GetResource(_viewResourceField.Value.KeyRule.GetResourceKey(View.GetType(), null), true));
-            }
-
-            foreach (var res in _resourceFields)
-            {
-                SetResource(res.FieldInfo, res.PropertyInfo, ResourceHandler.GetResource(res.KeyRule.GetResourceKey(View.GetType(), res.FieldInfo.Name), true));
-            }
-        }
-
-        private void SetResource(FieldInfo fieldInfo, PropertyInfo propInfo, string text)
-        {
-            // if the text is empty then there was no resource found and then do not set the resource!
-            if (string.IsNullOrEmpty(text))
-                return;
-
-            object field = fieldInfo.GetValue(View);
-
-            if (field == null)
-                return;
-
-            propInfo.SetValue(field, text);
+            _ruleExecutor?.Dispose();
         }
 
         #endregion
---
+++ src/Yggdrasil/View/RuleExecutor.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Yggdrasil.Resource;
+
+namespace Yggdrasil
+{
+    /// <summary>
+    /// Class for searching matching rules between view and view model or textresource rules...
+    /// Creates found links, set's resources...
+    /// </summary>
+    class RuleExecutor : IDisposable
+    {
+        #region Private Fields
+
+        private readonly List<ILinker> _linkers = new List<ILinker>();
+
+        #endregion
+
+        #region Public Methods
+
+        public void CreateLinks(object view, object context)
+        {
+            //if no rules defined for links --> nothing to do
+            if (!RuleProvider.AreLinkRulesDefined)
+                return;
+
+            EnumerateFields(view.GetType(), info => CreateLink(info.GetValue(view), context, info.Name));
+        }
+
+        public void SetResources(object view)
+        {
+            //if no rules defined for textresources --> nothing to do
+            if (!RuleProvider.AreTextResourceKeyRulesDefined)
+                return;
+
+            Type viewType = view.GetType();
+            TextResourceKeyRule rule = RuleProvider.GetTextResourceKeyRule(viewType);
+
+            if (rule != null)
+            {
+                SetResource(viewType, view, null, viewType);
+            }
+
+            EnumerateFields(viewType, info =>
+            {
+                SetResource(info.FieldType, info.GetValue(view), info.Name, viewType);
+            });
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void EnumerateFields(Type type, Action<FieldInfo> fieldAction)
+        {
+            foreach (FieldInfo info in type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                fieldAction(info);
+            }
+        }
+
+        private void CreateLink(object control, object context, string controlName)
+        {
+            if (control == null)
+                return;
+
+            List<LinkRule> linkRules = RuleProvider.GetLinkRulesForType(control.GetType());
+
+            if (linkRules.Count <= 0)
+                return;
+
+            ILinker linker = LinkerManager.GetLinkerForType(control.GetType());
+
+            if (linker == null)
+                return;
+
+            Link(control, context, linkRules, linker, controlName);
+        }
+
+        private void Link(object control, object context, IEnumerable<LinkRule> linkRules, ILinker linker, string controlName)
+        {
+            Type contextType = context?.GetType();
+
+            Dictionary<string, MemberInfo> foundLinks = new Dictionary<string, MemberInfo>();
+            Dictionary<string, string> linkDefinitions = new Dictionary<string, string>();
+
+            foreach (LinkRule rule in linkRules)
+            {
+                MemberInfo info = null;
+                string linkInfoName = rule.GetLinkInfoName(controlName);
+
+                switch (rule.RuleType)
+                {
+                    case LinkRuleType.Property:
+                        info = !string.IsNullOrEmpty(linkInfoName) ? contextType?.GetProperty(linkInfoName) : null;
+                        break;
+                    case LinkRuleType.Event:
+                        info = contextType?.GetMethod(linkInfoName);
+                        break;
+                }
+
+                linkDefinitions.Add(rule.InfoName, linkInfoName);
+                if (info != null)
+                    foundLinks.Add(rule.InfoName, info);
+            }
+
+            _linkers.Add(linker);
+            linker.Link(control, context, linkDefinitions, foundLinks, CreateLink);
+        }
+
+        //TODO continue implementing to link properties in sub classes if the wished property could not be found in 
+        //the passed context.
+        //private MemberInfo GetMemberInfo(object context, LinkRuleType linkType, string infoName)
+        //{
+        //    MemberInfo info = null;
+
+        //    switch (linkType)
+        //    {
+        //        case LinkRuleType.Property:
+        //            break;
+        //        case LinkRuleType.Event:
+        //            break;
+        //    }
+
+
+        //}
+
+        private void SetResource(Type type, object control, string controlName, Type viewType)
+        {
+            TextResourceKeyRule rule = RuleProvider.GetTextResourceKeyRule(type);
+
+            if (rule == null)
+                return;
+
+            PropertyInfo pInfo = type.GetProperty(rule.ResourcePropertyName);
+
+            if (pInfo == null)
+                return;
+
+            pInfo.SetValue(control, ResourceHandler.GetResource(rule.GetResourceKey(viewType, controlName), true));
+        }
+
+        #endregion
+
+        #region Interface Implementation
+
+        public void Dispose()
+        {
+            foreach (ILinker linker in _linkers)
+            {
+                linker.Unlink();
+            }
+        }
+
+        #endregion
+    }
+}
